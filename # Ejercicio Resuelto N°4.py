e7721e39c5290a28c48512e236439f262205c5c1
Caliber66 <127344630+Caliber66@users.noreply.github.com>
2023-08-22 20:41:16 -0500

    Add files via upload

---
+++ # Ejercicio Resuelto N°4.py
@@ -0,0 +1,7 @@
+# Ejercicio Resuelto N°4 
+
+EdadJuan = int(input('Edad de Juan '))
+EdadAlber = int(2 * EdadJuan /3)
+EdadAna = int(4/3 * EdadJuan)
+EDADMAMA = int(EdadAlber + EdadAna + EdadJuan)
+print('Las edades son: Alberto = ' + str(EdadAlber), 'Ana = ' + str(EdadAna), 'Juan = ' + str(EdadJuan), 'Mamá = ' + str(EDADMAMA))
