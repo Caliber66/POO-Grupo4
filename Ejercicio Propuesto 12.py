a6d80bc86bd90c775890722fb91f57508ba4665b
Caliber66 <daniel.rf2766@gmail.com>
2023-08-24 22:36:09 -0500

    commit name

--- Ejercicio Propuesto 12.py
+++ Ejercicio Propuesto 12.py
@@ -1,4 +1,4 @@
-# Un empleado trabaja 48 horas en la semana a razón de $5.000 hora. El porcentaje de retención en la fuente es del 12,5% del salario bruto. Se desea saber cuál es el salario bruto, la retención en la fuente y el salario neto del trabajador. 
+# Un empleado trabaja 48 horas en la semana a razón de $5.000 hora. El porcentaje de retención en la fuente es del 12,5% del salario bruto. Se desea saber cuál es el salario bruto, la retención en la fuente y el salario neto del trabajador.
 
 Horas = int(input('Horas trabajadas: '))
 PagoxHora = int(input('Pago por hora: '))
