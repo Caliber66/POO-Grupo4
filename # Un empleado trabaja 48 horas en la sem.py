33bb40423b2ff8137e7ba5d55fe92fd49b9c97ff
Caliber66 <daniel.rf2766@gmail.com>
2023-08-24 23:34:22 -0500

    Revert "1"
    This reverts commit 81cb97301c58f2eab42abfc560456ed7609454c7.

--- # Un empleado trabaja 48 horas en la sem.py
+++
@@ -1,9 +0,0 @@
-# Un empleado trabaja 48 horas en la semana a razón de $5.000 hora. El porcentaje de retención en la fuente es del 12,5% del salario bruto. Se desea saber cuál es el salario bruto, la retención en la fuente y el salario neto del trabajador.
-
-Horas = int(input('Horas trabajadas: '))
-PagoxHora = int(input('Pago por hora: '))
-Salario = Horas * PagoxHora
-Retencion = int((0.125*Salario))
-Neto = int((Salario-Retencion))
-
-print('Salario bruto:', '$'+str(Salario), 'Retención en la fuente: ', '$'+str(Retencion), 'Neto en la fuente: ', '$'+str(Neto)) 
