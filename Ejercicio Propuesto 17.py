5f785c8e299b05de8814aacf2110e1cbf3eb7f8b
Caliber66 <daniel.rf2766@gmail.com>
2023-08-24 21:53:02 -0500

    commit update to everything and rework

---
+++ Ejercicio Propuesto 17.py
@@ -0,0 +1,6 @@
+# 17. Dado el radio de un círculo. Haga un algoritmo que obtenga el área del círculo y la longitud de la circunferencia.
+
+Radio = int(input('Radio de el circulo: '))
+Area = (3.141592*(Radio**2))
+Circunferencia = ((2*3.141592)*Radio)
+print('El area es:', str(Area), 'La circunferencia es:', str(Circunferencia))
