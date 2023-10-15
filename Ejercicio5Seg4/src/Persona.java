b0e6c077c7e64a55b669b6e64224e5ddd8a96929
Caliber66 <daniel.rf2766@gmail.com>
2023-10-14 20:15:53 -0500

    ..

---
+++ Ejercicio5Seg4/src/Persona.java
@@ -0,0 +1,20 @@
+package Personas;
+
+/**
+ *
+ * @author danie
+ */
+public class Persona {
+    String nombre;
+    String apellidos;
+    String telefono;
+    String direccion;
+
+    public Persona(String nombre, String apellidos, String telefono, String direccion){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.direccion = direccion;
+
+    }
+}
