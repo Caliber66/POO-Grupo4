09447ff3ba17b72e2b68e96edfd087fe391b00dd
Caliber66 <daniel.rf2766@gmail.com>
2023-11-27 19:57:36 -0500

    :)

---
+++ Actividad6POO/src/ContactFriends/POOSeg6.java
@@ -0,0 +1,18 @@
+package pooseg6;
+
+/**
+ *
+ * @author danie
+ */
+public class POOSeg6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormAmigos frm = new FormAmigos();
+        frm.setVisible(true);
+
+    }
+
+}
